c96e4ccdff2cfacadedb87b88a1450b60bfbbdda
bzeyveli <41551142+bzeyveli@users.noreply.github.com>
2021-10-02 16:04:56 +0300

    runner düzeltildi

--- creational-design-patterns/src/com/example/creationaldesignpatterns/singleton/runner/Runner.java
+++ creational-design-patterns/src/com/example/creationaldesignpatterns/singleton/runner/Runner.java
@@ -11,7 +11,7 @@ public class Runner {
 		Connection connection2 = JdbcUtil.getConnection();
 		boolean isSame = (connection1 == connection2);
 		boolean isNotSame = (connection1 != connection2);
-		boolean isNotSame = (connection1 != connection2);
+		boolean isNotSame2 = (connection1 != connection2);
 		System.out.println("isSame : " + isSame);
 	}
 
